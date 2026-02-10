5716ea30a9f57ad03e9454c5c976a48fe52a118c
rlatjddms <kseo_o@naver.com>
2026-02-10 14:39:16 +0900

    [feat] 로그인 아이디에 따른 화면 이동

--- app/src/main/java/com/example/chain_g/LoginActivity.java
+++ app/src/main/java/com/example/chain_g/LoginActivity.java
@@ -2,7 +2,7 @@ package com.example.chain_g;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -24,7 +24,22 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
 
+        TextView loginButton = findViewById(R.id.login_btn);
         TextView findAccountButton = findViewById(R.id.find_account_btn);
+
+        EditText id = findViewById(R.id.id);
+        EditText password = findViewById(R.id.password);
+
+        loginButton.setOnClickListener(v -> {
+
+            if(id.equals("fac")) {
+                Intent intent = new Intent(LoginActivity.this, FacManagerMainActivity.class);
+                startActivity(intent);
+            } else if (id.equals("fran")) {
+                Intent intent = new Intent(LoginActivity.this, FranManagerMainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         findAccountButton.setOnClickListener(v -> {
             Intent intent = new Intent(LoginActivity.this, FindAccountActivity.class);
