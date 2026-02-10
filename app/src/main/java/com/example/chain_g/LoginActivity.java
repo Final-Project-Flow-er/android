e472fc034043f908610209e2a74c861b29095e5d
rlatjddms <kseo_o@naver.com>
2026-02-10 09:16:25 +0900

    [feat] 로그인 화면 구현

--- app/src/main/java/com/example/chain_g/LoginActivity.java
+++ app/src/main/java/com/example/chain_g/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.chain_g;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,13 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView findAccountButton = findViewById(R.id.find_account_btn);
+
+        findAccountButton.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, FindAccountActivity.class);
+            startActivity(intent);
         });
     }
 }
