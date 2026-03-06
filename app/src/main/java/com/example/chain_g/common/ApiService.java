1304a608cc0d70707fbb6ad98af53527ea8ff810
rlatjddms <kseo_o@naver.com>
2026-03-06 12:13:31 +0900

    [feat] #3 ApiService 추가

---
+++ app/src/main/java/com/example/chain_g/common/ApiService.java
@@ -0,0 +1,15 @@
+package com.example.chain_g.common;
+
+import com.example.chain_g.auth.dto.request.LoginRequest;
+import com.example.chain_g.auth.dto.response.LoginResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiService {
+
+    // 로그인
+    @POST("/api/v1/auth/login")
+    Call<ApiResponse<LoginResponse>> login(@Body LoginRequest request);
+}
